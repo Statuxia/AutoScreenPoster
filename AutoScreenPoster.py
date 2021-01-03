a86eb4073c4680cfa856a5f3cda193c9270fb933
FQGM <60938251+Statuxia@users.noreply.github.com>
2021-01-03 18:21:41 +0200

    Update AutoScreenPoster.py

--- AutoScreenPoster.py
+++ AutoScreenPoster.py
@@ -10,22 +10,45 @@ log = fr"C:\Users\{login}\AppData\Roaming\.vimeworld\minigames\logs\latest.log"
 client_id = 'you_imgur_client_id'
 client_secret = 'you_imgur_secret_id'
 client = ImgurClient(client_id, client_secret)
+print("""
+
+ █████╗     ██████╗   ██████╗    
+██╔══██╗   ██╔════╝   ██╔══██╗   
+███████║   ╚█████╗    ██████╔╝   
+██╔══██║    ╚═══██╗   ██╔═══╝    
+██║  ██║██╗██████╔╝██╗██║
+╚═╝  ╚═╝╚═╝╚═════╝ ╚═╝╚═╝
+
+       ╔╗  ╔╗   ╔══╦══╦══╦═╦═╗
+╔══╦═╗╔╝╠═╗║╚╦╦╗║═╦╣╔╗║╔═╣║║║║
+║║║║╬╚╣╬║╩╣║╬║║║║╔╝║╚╝║╚╗║║║║║
+╚╩╩╩══╩═╩═╝╚═╬╗║╚╝ ╚═╗╠══╩╩═╩╝
+             ╚═╝     ╚╝
+""")
+
+print("""Как пользоваться:
+Заходите на VimeWorld;
+Нажимаете F2;
+Ждете...
+Получаете ссылку на ваш скриншот.
+""")
+
 while True:
-    try:
-        if is_pressed("F2"):
-            screen_true = False
-            start_time = time()
-            while not screen_true:
-                f = open(log, 'r')
-                all_lines = f.readlines()
-                for i in range(1, 3):
-                    if "Снимок экрана сохранён как" in all_lines[i * -1]:
-                        screen_true = True
-                f.close()
-            path = fr"{screenshots}\{listdir(screenshots)[-1]}"
-            items = client.upload_from_path(path, config=None, anon=True)
-            copy(items["link"])
-            Beep(300, 100)
-            Beep(200, 50)
-    except:
-        continue
+    if is_pressed("F2"):
+        screen_true = False
+        start_time = time()
+        while not screen_true:
+            f = open(log, 'r', encoding='utf-8')
+            all_lines = f.readlines()
+            for i in range(1, 3):
+                if "Снимок экрана сохранён как" in all_lines[i * -1]:
+                    screen_true = True
+            f.close()
+        path = fr"{screenshots}\{listdir(screenshots)[-1]}"
+        print(f"Скриншот {str(listdir(screenshots)[-1])} найден.")
+        items = client.upload_from_path(path, config=None, anon=True)
+        print("Скриншот отправлен на imgur.")
+        copy(items["link"])
+        print("Ссылка скопирована.")
+        Beep(300, 100)
+        Beep(200, 50)
